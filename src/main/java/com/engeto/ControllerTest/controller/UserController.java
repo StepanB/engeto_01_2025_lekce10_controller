2d4e5b9bba110ae7ba80f4a8e1f97a8c2969f502
stepan-bohm <stepanbohm@gmail.com>
2025-03-25 09:18:28 +0100

    initial commit

---
+++ src/main/java/com/engeto/ControllerTest/controller/UserController.java
@@ -0,0 +1,53 @@
+package com.engeto.ControllerTest.controller;
+
+import com.engeto.ControllerTest.dto.User;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private List<User> users = new ArrayList<>();
+
+    private void createDefaultUsers() {
+        User user = new User();
+        user.setName("Stepan");
+        user.setLastName("Bohm");
+        user.setUserId(1L);
+
+        User user2 = new User("Karel", "Novak", 2L, "Kaja");
+        users.add(user);
+        users.add(user2);
+
+    }
+
+    @GetMapping("create")
+    public void createUsers() {
+        createDefaultUsers();
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return users;
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable("id") Long id) {
+        for (User u : users) {
+            if (u.getUserId().equals(id)) {
+                return u;
+            }
+        }
+        return null;
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        users.add(user);
+
+        return user;
+    }
+}
